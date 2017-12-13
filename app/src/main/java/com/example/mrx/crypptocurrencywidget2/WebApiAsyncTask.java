160dd93483c0c06f8fad7052914bbe66843ad2f9
mrx <tobjo057@student.liu.se>
2017-12-13 11:53:26 +0100

    WebApi AsyncTask created and "finished"

---
+++ app/src/main/java/com/example/mrx/crypptocurrencywidget2/WebApiAsyncTask.java
@@ -0,0 +1,76 @@
+package com.example.mrx.crypptocurrencywidget2;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
+
+/**
+ * Created by mrx on 2017-12-13.
+ */
+
+public class WebApiAsyncTask extends AsyncTask<Void, Void, String> {
+    public static final String STRING_URL = "https://api.coinmarketcap.com/v1/ticker/";
+    public static final int CONNECT_TIME = 15000;
+    public static final int READ_TIME = 15000;
+    public static final String REQUEST_METHOD = "GET";
+
+    private Context context;
+
+    public WebApiAsyncTask(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    protected String doInBackground(Void... voids) {
+        String jsonString = null;
+        String input = null;
+        try {
+            URL url = new URL(STRING_URL);
+            HttpsURLConnection urlConnection = (HttpsURLConnection) url.openConnection();
+            urlConnection.setRequestMethod(REQUEST_METHOD);
+            urlConnection.setReadTimeout(READ_TIME);
+            urlConnection.setConnectTimeout(CONNECT_TIME);
+            urlConnection.connect();
+
+            BufferedReader bufferedReader = new BufferedReader(
+                    new InputStreamReader(urlConnection.getInputStream()));
+            StringBuilder stringBuilder = new StringBuilder();
+            while ((input = bufferedReader.readLine())!= null){
+                stringBuilder.append(input);
+            }
+            bufferedReader.close();
+            jsonString = stringBuilder.toString();
+
+        } catch (MalformedURLException e) {
+            Toast.makeText(context, "MalformedURLException", Toast.LENGTH_SHORT);
+        } catch (IOException e) {
+            Toast.makeText(context, "IOException", Toast.LENGTH_SHORT);
+        }
+        return jsonString;
+    }
+
+    @Override
+    protected void onPostExecute(String response) {
+        super.onPostExecute(response);
+        JSONArray jsonArray;
+
+        if (response != null){
+            try {
+                jsonArray = new JSONArray(response);
+            } catch (JSONException e) {
+                Toast.makeText(context, "JSONException", Toast.LENGTH_SHORT);
+            }
+        }
+    }
+}
